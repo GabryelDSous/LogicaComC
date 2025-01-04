0e86fb52aa23438385df637fcf7a7c8f05843d25
unknown <gabryelpro260406@gmail.com>
2025-01-04 19:52:45 -0300

    terminado aula001

--- Aula001/helloworld.c
+++ Aula001/helloworld.c
@@ -1,7 +1,12 @@
-#include <stdio.h>
-#include <stdlib.h>
-
+#include <stdio.h>;
+#include <stdlib.h>;
 int main(){
     printf("Hello, World!\n");
+    printf(somar());
     system("pause");
+}
+
+float Somar(){
+    float a = 5.2 + 4.8;
+    return a;
 }
